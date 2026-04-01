ab2c05774c7f0f91001d9f00d111a0de536c6a54
harrison opondo otieno <159556298+Meatycrude@users.noreply.github.com>
2026-04-01 14:48:34 +0300

    Initial commit: Add PHP folder

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php system</title>
+</head>
+<body>
+    <h1>php system</h1>
+    <p>php system is a simple php framework for learning and building small projects.</p>   
+</body>
+</html>
