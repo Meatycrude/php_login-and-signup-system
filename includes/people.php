c234b5105b446f4817b46ba795ec99463a2a9ec8
harrison opondo otieno <159556298+Meatycrude@users.noreply.github.com>
2026-04-04 18:38:02 +0300

    peoples class with two include classes

---
+++ includes/people.php
@@ -0,0 +1,21 @@
+<?php
+
+class People{
+   
+     public $name = "harrison";
+     public $age = 20;
+     public $gender = "male";
+}
+
+class NewClass{
+
+   public function owner(){
+
+   $a = "harrison";
+   $b = "otieno";
+   $c = "harrison otieno";
+
+   
+  return $c; }
+
+}
