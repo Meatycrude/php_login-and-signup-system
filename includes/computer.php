591fa3c779bfcf6485e9c6d609c74c896f0ff09a
harrison opondo otieno <159556298+Meatycrude@users.noreply.github.com>
2026-04-06 14:29:43 +0300

    new contructor and destructor file

---
+++ includes/computer.php
@@ -0,0 +1,18 @@
+<?php
+
+class Computer{
+    public $name;
+    public $brand;
+    public $price;
+
+    public function __construct($name, $brand, $price){
+
+    $this->name = $name;
+    $this->brand = $brand;
+    $this->price = $price;
+
+
+    }
+
+
+}
