be1f5f2d97e500d6e7fe59455cc6d68afafcb84d
harrison opondo otieno <159556298+Meatycrude@users.noreply.github.com>
2026-04-06 14:59:42 +0300

    the static property is  called in the index file

--- index.php
+++ index.php
@@ -11,19 +11,14 @@
     include "includes/people.php";
     include "includes/person.php";
     include "includes/computer.php";
+    include "drink.php";
     
 
     $computer1= new Computer("Macbook Pro", "Apple", 2000);
     $computer2= new Computer("Surface Laptop", "Microsoft", 1500);
     $person1 = new person("John", 20, "Nairobi");
-    unset($person1);
-
-    echo $person1->name;
-    echo "<br>";
-    echo $person1->location;
-    echo "<br>";
-    echo $person1->age;
-    echo "<br>";
+     
+    echo Drink::$drinkingAge;
 
     echo $computer1->name;
     echo "<br>";
