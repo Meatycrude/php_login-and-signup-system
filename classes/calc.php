bbd9ca3f75039889baf2a7ba2f89924741927342
harrison opondo otieno <159556298+Meatycrude@users.noreply.github.com>
2026-04-08 14:24:52 +0300

    whole function to calculate the user input

---
+++ classes/calc.php
@@ -0,0 +1,39 @@
+<?php
+
+class Calc{
+    public $operator;
+    public $num1;
+    public $num2;
+
+    public function __construct(string $operator, float $num1, float $num2) {
+        $this->operator = $operator;
+        $this->num1 = $num1;
+        $this->num2 = $num2;
+    }
+
+    public function calculate() {
+      
+    switch($this->operator) {
+
+     case "+":
+        return $this->num1 + $this->num2;
+
+     case "-":
+        return $this->num1 - $this->num2;
+
+     case "*":
+        return $this->num1 * $this->num2;
+
+    case "/":
+        if($this->num2 == 0) {
+            return "Error: Division by zero";
+        }
+        return $this->num1 / $this->num2;
+
+     default:
+        return "Invalid operator";
+    }
+
+
+}
+}
