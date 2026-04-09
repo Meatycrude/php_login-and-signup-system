8be447d3869372b1b5bed2f078c3ce2ea9142774
harrison opondo otieno <159556298+Meatycrude@users.noreply.github.com>
2026-04-09 21:21:08 +0300

    has to require the class clac in an attempt to debug the code

--- includes/class.inc.php
+++ includes/class.inc.php
@@ -1,4 +1,18 @@
 <?php
-include_once 'class-autoload.inc.php';
-declare(strict_types=1);
-?>
+require "classes/calc.php";
+
+$operator = $_POST['operator'] ?? '';
+$num1 = $_POST['num1'] ?? 0;
+$num2 = $_POST['num2'] ?? 0;    
+
+$calc = new Calc($operator, (float)$num1, (float)$num2);
+
+try{
+    echo $calc->calc();
+}catch(TypeError $e) {
+    echo "Error: " . $e->getMessage();
+}
+
+
+
+
